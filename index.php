65ae7b658ce8d4052186cc714cf13f3d3cd2ede5
Panda359 <jiewjaw@gmail.com>
2017-06-20 12:43:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		
 		
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text']=='random')
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text']=='random x')
 		{
 		$text = $event['message']['text'];
 			
