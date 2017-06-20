ae9553c46a811dd9f31d833d0a82927391055bab
Panda359 <jiewjaw@gmail.com>
2017-06-20 12:12:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,10 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text,
-				 "type": "sticker",
- 				 "packageId": "1",
- 				 "stickerId": "1"
-			];
+				'type' => 'sticker',
+				'packageId' => '1',
+				'stickerId' => '1'
+					];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
