fbdabd47e994c76269703211449e8af6fb94d8f4
Panda359 <jiewjaw@gmail.com>
2017-06-20 12:08:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,
+				 "type": "sticker",
+ 				 "packageId": "1",
+ 				 "stickerId": "1"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
