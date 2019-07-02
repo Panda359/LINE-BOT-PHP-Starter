35415d48387b825a5d3f3ab91dda35e7959d19af
Panda359 <jiewjaw@gmail.com>
2019-07-02 16:50:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2863,6 +2863,49 @@ $i[104]="อาหารตามสั่งงามตา";
 
 
 	foreach ($events['events'] as $event) {
+		
+		if ($event['type'] == 'leave'
+		    
+		    {
+		    
+		 
+			// Build message to reply back
+	
+			$messages3 = [
+				
+			
+				'type' => 'text',
+				'text' => 'Bye Bye :)'
+				
+					];
+			
+			
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages3],
+				
+				
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+		    
+		    }
+		
 		// Reply only when message sent is in 'text' format
 	
 		$text_mes=$event['message']['text'];
