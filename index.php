0831350f0866f3a342254606f401374dcd1b20a6
Panda359 <jiewjaw@gmail.com>
2019-07-02 17:09:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2864,45 +2864,6 @@ $i[104]="อาหารตามสั่งงามตา";
 
 	foreach ($events['events'] as $event) {
 		
-		if ($event['type'] == 'join'
-		    
-		    {
-		    
-		 
-			// Build message to reply back
-	
-			$messages3 = [
-				
-			
-				'type' => 'text',
-				'text' => 'Welcome'
-				
-					];
-			
-			
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages3],
-				
-				
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
 		    
 		    
 		
