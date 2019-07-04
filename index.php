d512ffda2b781a801497b17a72cfcf114cfcf8e4
Panda359 <jiewjaw@gmail.com>
2019-07-04 11:38:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3748,62 +3748,60 @@ $i[104]="อาหารตามสั่งงามตา";
 				
 				
 				
-				
-  "type" => "text",
-  "text" => "Hello"
+
 
 				
 				
-/*			
-"type": "flex",
-  "altText": "Flex Message",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "header": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
+			
+"type" => "flex",
+  "altText" => "Flex Message",
+  "contents" => {
+    "type" => "bubble",
+    "direction" => "ltr",
+    "header" => {
+      "type" => "box",
+      "layout" => "vertical",
+      "contents" => [
         {
-          "type": "text",
-          "text": "Header",
-          "align": "center"
+          "type" => "text",
+          "text" => "Header",
+          "align" => "center"
         }
       ]
     },
-    "hero": {
-      "type": "image",
-      "url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
-      "size": "full",
-      "aspectRatio": "1.51:1",
-      "aspectMode": "fit"
+    "hero" => {
+      "type" => "image",
+      "url" => "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+      "size" => "full",
+      "aspectRatio" => "1.51:1",
+      "aspectMode" => "fit"
     },
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
+    "body" => {
+      "type" => "box",
+      "layout" => "vertical",
+      "contents" => [
         {
-          "type": "text",
-          "text": "Body",
-          "align": "center"
+          "type" => "text",
+          "text" => "Body",
+          "align" => "center"
         }
       ]
     },
-    "footer": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
+    "footer" => {
+      "type" => "box",
+      "layout" => "horizontal",
+      "contents" => [
         {
-          "type": "button",
-          "action": {
-            "type": "uri",
-            "label": "Button",
-            "uri": "https://linecorp.com"
+          "type" => "button",
+          "action" => {
+            "type" => "uri",
+            "label" => "Button",
+            "uri" => "https://linecorp.com"
           }
         }
       ]
     }
-  }	*/	
+  }		
 				
 					];
 			
