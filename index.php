44fe9d20cd126dea3d677a73259c1f286044fb85
Panda359 <jiewjaw@gmail.com>
2019-07-04 11:29:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3746,22 +3746,28 @@ $i[104]="อาหารตามสั่งงามตา";
 			//	'type' => 'text',
 			//	'text' => $text
 			
-				 "type": "flex",
+"type": "flex",
   "altText": "Flex Message",
   "contents": {
     "type": "bubble",
     "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "Header",
+          "align": "center"
+        }
+      ]
+    },
     "hero": {
       "type": "image",
-      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      "url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
       "size": "full",
-      "aspectRatio": "20:13",
-      "aspectMode": "cover",
-      "action": {
-        "type": "uri",
-        "label": "Line",
-        "uri": "https://linecorp.com/"
-      }
+      "aspectRatio": "1.51:1",
+      "aspectMode": "fit"
     },
     "body": {
       "type": "box",
@@ -3769,138 +3775,26 @@ $i[104]="อาหารตามสั่งงามตา";
       "contents": [
         {
           "type": "text",
-          "text": "Brown Cafe",
-          "size": "xl",
-          "weight": "bold"
-        },
-        {
-          "type": "box",
-          "layout": "baseline",
-          "margin": "md",
-          "contents": [
-            {
-              "type": "icon",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-              "size": "sm"
-            },
-            {
-              "type": "icon",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-              "size": "sm"
-            },
-            {
-              "type": "icon",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-              "size": "sm"
-            },
-            {
-              "type": "icon",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-              "size": "sm"
-            },
-            {
-              "type": "icon",
-              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-              "size": "sm"
-            },
-            {
-              "type": "text",
-              "text": "4.0",
-              "flex": 0,
-              "margin": "md",
-              "size": "sm",
-              "color": "#999999"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "margin": "lg",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "Place",
-                  "flex": 1,
-                  "size": "sm",
-                  "color": "#AAAAAA"
-                },
-                {
-                  "type": "text",
-                  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                  "flex": 5,
-                  "size": "sm",
-                  "color": "#666666",
-                  "wrap": true
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "Time",
-                  "flex": 1,
-                  "size": "sm",
-                  "color": "#AAAAAA"
-                },
-                {
-                  "type": "text",
-                  "text": "10:00 - 23:00",
-                  "flex": 5,
-                  "size": "sm",
-                  "color": "#666666",
-                  "wrap": true
-                }
-              ]
-            }
-          ]
+          "text": "Body",
+          "align": "center"
         }
       ]
     },
     "footer": {
       "type": "box",
-      "layout": "vertical",
-      "flex": 0,
-      "spacing": "sm",
+      "layout": "horizontal",
       "contents": [
         {
           "type": "button",
           "action": {
             "type": "uri",
-            "label": "CALL",
+            "label": "Button",
             "uri": "https://linecorp.com"
-          },
-          "height": "sm",
-          "style": "link"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "uri",
-            "label": "WEBSITE",
-            "uri": "https://linecorp.com"
-          },
-          "height": "sm",
-          "style": "link"
-        },
-        {
-          "type": "spacer",
-          "size": "sm"
+          }
         }
       ]
     }
-  }
-				
+  }		
 				
 					];
 			
